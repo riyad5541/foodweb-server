c9536974a488fef2e35ec657d9389e131a7bb35e
riyad5541 <abdullahalriyad5541@gmail.com>
2023-02-28 15:53:24 +0600

    install jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
